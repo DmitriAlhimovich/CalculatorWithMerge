c4710a62ffd1a4708658d7e45db92a63e9cc1be2
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-07-09 11:44:33 +0300

    Add project files.

---
+++ CalculatorWithMergerRebase/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CalculatorWithMergerRebase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number:");
+            var number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input operation (+, -):");
+
+            var operation = Console.ReadLine()[0];
+
+            Console.WriteLine("Input number:");
+            
+            var number2 = int.Parse(Console.ReadLine());
+
+            int result;
+
+
+            switch (operation)
+            {
+                case '+': 
+                result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                default:
+                    throw new ArgumentException($"Operation not supported: {operation}");
+            }
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
